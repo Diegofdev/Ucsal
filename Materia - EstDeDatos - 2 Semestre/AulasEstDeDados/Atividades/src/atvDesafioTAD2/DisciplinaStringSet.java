13a3cbc6b9a5b8fbc3a678ca10e7d7bfb00779d5
Diegofdev <dfsantosba@gmail.com>
2020-08-25 21:37:02 -0300

    Update DisciplinaStringSet.java

--- Materia - EstDeDatos - 2 Semestre/AulasEstDeDados/Atividades/src/atvDesafioTAD2/DisciplinaStringSet.java
+++ Materia - EstDeDatos - 2 Semestre/AulasEstDeDados/Atividades/src/atvDesafioTAD2/DisciplinaStringSet.java
@@ -9,14 +9,16 @@ public class DisciplinaStringSet implements DisciplinaSet {
 	private String curso;
 	private String instituicao;
 	private String professor;
+	private String semestre;
 	
-	public DisciplinaStringSet (String[] nome, int horas, String curso, String instituicao, String professor) {
+	public DisciplinaStringSet (String[] nome, int horas, String curso, String instituicao, String professor, String semestre) {
 		super();
 		this.nome = nome;
 		this.horas = horas;
 		this.instituicao = instituicao;
 		this.curso = curso;
 		this.professor = professor;
+		this.semestre = semestre;
 	}
 	
 	@Override
